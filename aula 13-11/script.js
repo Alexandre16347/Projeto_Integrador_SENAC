7aad15dd791a3dd8301a86258082a31146c4db7f
felipe silva 7 <fs0987145@gmail.com>
2023-11-14 21:26:05 -0300

    criação do card
    criação do card pelo js utilizando o fragment

--- aula 13-11/script.js
+++ aula 13-11/script.js
@@ -1,16 +1,55 @@
-let container = document.getElementById("container")
 
-container.innerHTML = `
+// let texto = "Indice:";
 
-<div class="card">
+// for (let i = 0; i < 50; i++) {
+//   let card = `
 
-<div class="top">
+// <div class="card">
 
-</div>
+// <div class="top">
+// ${texto + i}
 
-<div class="botton">
+// </div>
 
-</div>
+// <div class="botton">
+
+// </div>
+
+// `
+//   container.innerHTML += card;
+//  nao usar inner/ falta de segurança}
 
 
-`
+
+
+let container = document.getElementById("container");
+
+let fragment = document.createDocumentFragment()
+
+let card = document.createElement('div')
+card.setAttribute('class', 'card')
+
+let div_img = document.createElement('div')
+div_img.setAttribute('class', 'div-img')
+
+card.append(div_img)
+
+let div_chef = document.createElement('div')
+div_chef.setAttribute('class', 'chef-avatar')
+
+card.append(div_chef)
+
+
+
+
+
+fragment.append(card)
+
+
+
+
+
+
+
+
+container.append(fragment)
